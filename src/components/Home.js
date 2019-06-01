7049b22cd62bf996a23a452abc18c5996c3c14ff
lmilliken <millifly@gmail.com>
2019-06-01 01:17:17 -0600

    cookie session test

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+const axios = require('axios');
 
 export default class Home extends Component {
+  myProfile = async () => {
+    const response = await axios.get('/auth/current_user', {
+      withCredentials: true
+    });
+
+    console.log('profile: ', response);
+  };
+
   render() {
-    return <div>Home</div>;
+    return (
+      <div>
+        <p>Home</p>
+        <button onClick={this.myProfile}>My profile</button>
+      </div>
+    );
   }
 }
